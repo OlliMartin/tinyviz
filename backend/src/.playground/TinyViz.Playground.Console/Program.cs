fa01e067424a977848fc413f27a870bc9b3d6efd
Olli Martin <oss@ollimart.in>
2026-05-04 05:14:41 +0200

    Create playground project

---
+++ backend/src/.playground/TinyViz.Playground.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Plotly.NET;
+using Plotly.NET.CSharp;
+using Chart = Plotly.NET.CSharp.Chart;
+using GenericChartExtensions = Plotly.NET.CSharp.GenericChartExtensions;
+
+GenericChartExtensions.Show(
+    GenericChartExtensions.WithTraceInfo(
+            Chart.Point<double, double, string>(
+                [1, 2,],
+                [5, 10,]
+            ),
+            "Hello from C#",
+            ShowLegend: true
+        )
+        .WithXAxisStyle<double, double, string>(Title: Title.init("xAxis"))
+        .WithYAxisStyle<double, double, string>(Title: Title.init("yAxis"))
+);
