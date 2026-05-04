9f1564562e31687b1b4cb695fce9912238de7f27
Olli Martin <oss@ollimart.in>
2026-05-04 05:16:25 +0200

    Reformat

--- backend/src/.playground/TinyViz.Playground.Console/Program.cs
+++ backend/src/.playground/TinyViz.Playground.Console/Program.cs
@@ -1,19 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Plotly.NET.CSharp;
 
-using Plotly.NET;
-using Plotly.NET.CSharp;
-using Chart = Plotly.NET.CSharp.Chart;
-using GenericChartExtensions = Plotly.NET.CSharp.GenericChartExtensions;
-
-GenericChartExtensions.Show(
-    GenericChartExtensions.WithTraceInfo(
-            Chart.Point<double, double, string>(
-                [1, 2,],
-                [5, 10,]
-            ),
-            "Hello from C#",
-            ShowLegend: true
-        )
-        .WithXAxisStyle<double, double, string>(Title: Title.init("xAxis"))
-        .WithYAxisStyle<double, double, string>(Title: Title.init("yAxis"))
-);
+Chart.Point<double, double, string>(
+        [1, 2,],
+        [5, 10,]
+    )
+    .WithTraceInfo("Hello from C#", ShowLegend: true)
+    .WithXAxisStyle<double, double, string>(Title: Plotly.NET.Title.init("xAxis"))
+    .WithYAxisStyle<double, double, string>(Title: Plotly.NET.Title.init("yAxis"))
+    .Show();
