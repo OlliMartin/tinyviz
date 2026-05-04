b81dfa687432b43bd04ba6dd399f96c68e33c6c1
Olli Martin <oss@ollimart.in>
2026-05-04 05:48:35 +0200

    setup husky

--- backend/src/.playground/TinyViz.Playground.Console/Program.cs
+++ backend/src/.playground/TinyViz.Playground.Console/Program.cs
@@ -1,9 +1,7 @@
 ﻿using Plotly.NET.CSharp;
 
-Chart.Point<double, double, string>(
-        [1, 2,],
-        [5, 10,]
-    )
+Chart
+    .Point<double, double, string>([1, 2], [5, 10])
     .WithTraceInfo("Hello from C#", ShowLegend: true)
     .WithXAxisStyle<double, double, string>(Title: Plotly.NET.Title.init("xAxis"))
     .WithYAxisStyle<double, double, string>(Title: Plotly.NET.Title.init("yAxis"))
