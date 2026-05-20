1db3fa48b339b7c67442166861f67b0889629bcb
Olli Martin <oss@ollimart.in>
2026-05-20 08:26:46 +0200

    Cleanup

--- backend/src/TinyViz.Templating/Internal/Nodes/KeyedPrimitiveNode.cs
+++ backend/src/TinyViz.Templating/Internal/Nodes/KeyedPrimitiveNode.cs
@@ -3,10 +3,4 @@ namespace TinyViz.Templating.Internal.Nodes;
 public record KeyedPrimitiveNode(string Key, object? Value) : KeyedNode(Key)
 {
     public override void SerializeInto(Dictionary<string, object?> dictionary) => dictionary.Add(Key, Value);
-
-    public override void SerializeInto(List<object?> list)
-    {
-        Console.WriteLine("wha");
-        base.SerializeInto(list);
-    }
 }
